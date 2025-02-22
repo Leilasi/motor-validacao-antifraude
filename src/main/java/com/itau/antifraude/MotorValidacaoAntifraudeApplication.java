2c97b394846d2a28e7be89def9c81877b69dfd5f
Leilasi <leilafernandadasilva1994@gmail.com>
2025-02-22 01:39:22 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/itau/antifraude/MotorValidacaoAntifraudeApplication.java
@@ -0,0 +1,13 @@
+package com.itau.antifraude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorValidacaoAntifraudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorValidacaoAntifraudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/antifraude/MotorValidacaoAntifraudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.antifraude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorValidacaoAntifraudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
